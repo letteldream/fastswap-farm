7d7bf18eb3d90f283e8570afa5a3db43356b75a3
IntegralTeam <repo@sfxdx.ru>
2021-05-18 15:28:15 +0700

    Change date

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -138,8 +138,8 @@ module.exports = function (deployer, network) {
       if (network == 'testnet') {
         start = 1621215629;
       } else if (network == 'bsc') {
-        // Tue May 18 2021 17:00:00 GMT+0000
-        start = 1621357200;
+        // Wed May 19 2021 17:00:00 GMT+0000
+        start = 1621443600;
       }
 
       const fast = await Fast.at(fastTokenAddress);
